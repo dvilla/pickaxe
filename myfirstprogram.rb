a39dedb506dd97201e2b3f1a8f75501b4451078c
newnowave <gdrapps@hotmail.com>
2017-12-06 21:01:39 -0700

    first excercie from pickaxe book

---
+++ myfirstprogram.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+puts "Hello!"
+
+puts "It is now #{Time.now}"
+
+#This is a very cool trick where we can invoke the script like ./script.rb instead of ruby script.rb
+#using the shebang and the path for the ruby binary
